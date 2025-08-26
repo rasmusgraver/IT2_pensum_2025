3bfd01fb72d17f88289e3e4fefadd1ea904dfbe3
Rasmus Graver <rasmus.graver@gmail.com>
2025-08-26 14:03:44 +0200

    Create 1A Moenstre.py

---
+++ Kap 1 Basic Python/1A Moenstre.py
@@ -0,0 +1,9 @@
+
+# Print en "pyramide"
+for i in range(7):
+    print("#"*(i+1))
+
+
+# Husker denne fra sist...
+for i in range(4):
+    print(i, end="") #printer 0123 uten mellomrom eller linjeskift
