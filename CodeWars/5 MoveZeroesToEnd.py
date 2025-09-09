cb705b4e65be51a40dc5627f7065de4014230bb2
Rasmus Graver <rasmus.graver@gmail.com>
2025-09-09 11:15:14 +0200

    Create 5 MoveZeroesToEnd.py

---
+++ CodeWars/5 MoveZeroesToEnd.py
@@ -0,0 +1,10 @@
+
+# https://www.codewars.com/kata/52597aa56021e91c93000cb0
+# => Trykk på knappen "train"
+
+def move_zeros(lst):
+
+    return lst
+
+
+print(move_zeros([1, 0, 1, 2, 0, 1, 3]) )  # Skal printe [1, 1, 2, 1, 3, 0, 0]
