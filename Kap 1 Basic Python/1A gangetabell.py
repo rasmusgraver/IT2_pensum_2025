b96f3e7f022f99a8bff589c2b5e9f618582033e9
Rasmus Graver <rasmus.graver@gmail.com>
2025-09-18 09:22:14 +0200

    gangetabell med funksjon

--- Kap 1 Basic Python/1A gangetabell.py
+++ Kap 1 Basic Python/1A gangetabell.py
@@ -1,7 +1,19 @@
 
-
+"""
 # Demo av dobbel for-løkke, og også dette med end= til print:
 for i in range(1, 10):
     for j in range(1, 10):
         print(f"{i*j:2}", end=" ")
     print()
+"""
+
+def skrivRad(tall):
+    for i in range(tall, 10*tall+1, tall):
+        if i < 10:
+            print(" ", end="")
+        print(i, end=" ")
+    print()
+
+# Skriv gangetabellen fra 1 til 9:
+for t in range(1, 10):
+    skrivRad(t)
