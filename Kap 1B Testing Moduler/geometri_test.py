fee68de9cccbfaf4ffdc9adbab1b0d4add689005
Rasmus Graver <rasmus.graver@gmail.com>
2025-10-22 09:07:03 +0200

    liten esktra test

--- Kap 1B Testing Moduler/geometri_test.py
+++ Kap 1B Testing Moduler/geometri_test.py
@@ -4,3 +4,10 @@ areal = arealAvSirkel(2)
 print(areal)
 if areal != (3.14*4):
     print("FEIL. Areal av sirkel med r=2")
+
+areal = arealAvSirkel(1)
+print(areal)
+if areal != (3.14):
+    print("FEIL. Areal av sirkel med r=1")
+
+    
