0dcda34e6ac42e184f69ac5eb3d1d9d0f89bc5c1
Rasmus Graver <rasmus.graver@gmail.com>
2025-11-04 16:11:55 +0100

    split strip

--- Kap 1 Basic Python/1x leseFraFil_del2.py
+++ Kap 1 Basic Python/1x leseFraFil_del2.py
@@ -4,12 +4,13 @@ with open('DataFiler/tall_liste.txt', 'r') as fil:
     # Les fila - EN LINJE av gangen
     for linje in fil:
         # linje = linje.strip()
-        tallene = linje.split("-")
+        tallene = linje.strip().split("-")
 
         print("Tallene:", tallene)
 
         total = 0
         for s in tallene:
+            # HUSK! Må gjøre om fra streng til tall!
             t = int(s)
             total += t
 
