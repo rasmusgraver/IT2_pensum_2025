f7a20d844bacadef2c1b8fc049777d43d5f257d6
Rasmus Graver <rasmus.graver@gmail.com>
2025-11-19 10:23:28 +0100

    Update main.py

--- Kap 2 OOP/Bondegård/main.py
+++ Kap 2 OOP/Bondegård/main.py
@@ -1,6 +1,7 @@
 from bondegård import Bondegård
 from dyr import *
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 # Opprett en bondegård: 
 gården = Bondegård()
@@ -31,7 +32,7 @@ for _ in range(20):
     ting.append(gården.samleTingFraDyrene())
 
 # Lag en liste med tallene 1,2,3,4...20:
-dager = list(range(20))
+dager = list(range(1,21))
 egg = []
 melk = []
 
@@ -39,7 +40,22 @@ for ordbok in ting:
     egg.append(ordbok["egg"])
     melk.append(ordbok["melk"])
 
-plt.plot(dager, egg)
-plt.plot(dager, melk)
+plt.plot(dager, egg, label="egg")
+
+# Ta kontroll over x-aksen:
+plt.xticks(dager[::2])
+
+# Skap en alternativ y-akse: (merk: gca betyr "get current axis")
+ax1 = plt.gca()
+ax1.set_ylabel("Egg")
+# Litt fancy syntax for å si at vi vil ha heltall y-ticks:
+ax1.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+# Hent en alternativ axe (tvilling-y-akse)
+ax2 = ax1.twinx()
+ax2.plot(dager, melk, color='orange', label="melk")
+ax2.set_ylabel("Melk (liter)", color='black')
+ax2.yaxis.set_major_locator(MaxNLocator(integer=True))
+
 plt.show()
 
