b26f9ebde60434c96ea4a655f4b447861a8980ed
Rasmus Graver <rasmus.graver@gmail.com>
2025-12-09 14:01:46 +0100

    Update 3 basis ballsprett.py

--- Kap 4 PyGame/3 basis ballsprett.py
+++ Kap 4 PyGame/3 basis ballsprett.py
@@ -13,31 +13,8 @@ pg.init()
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 clock = pg.time.Clock()
 
-class Ball:
-
-    def __init__(self, x, y, radius, farge) -> None:
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.farge = farge
-        self.vx = 5 # Fart i x-retning
-
-    def oppdater(self):
-        self.x += self.vx
-        if self.x > VINDU_BREDDE:
-            self.vx *= -1 # Snu farten (x-retning)
-        if self.x < 0:
-            self.vx *= -1 # Snu farten (x-retning)        
-        
-
-    def tegn(self, vindu):
-        # Tegne objektet på skjermen
-        pg.draw.circle(vindu, color=self.farge, center=(self.x, self.y), radius=self.radius)
-
-
 
 def main():
-    ball1 = Ball(100, 100, 40, RED)
     running = True
     while running:
         for event in pg.event.get():
@@ -47,9 +24,7 @@ def main():
                 running = False
 
         vindu.fill(WHITE)
-        ball1.oppdater()
-        ball1.tegn(vindu)
-
+        pg.draw.circle(vindu, RED, (100, 250), 50)
 
         pg.display.flip()
         clock.tick(FPS)
