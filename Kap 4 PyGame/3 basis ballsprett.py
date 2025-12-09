c4a497d5792b24d3ec6af279f844d23f456b6097
Rasmus Graver <rasmus.graver@gmail.com>
2025-12-09 12:08:28 +0100

    Create 3 basis ballsprett.py

---
+++ Kap 4 PyGame/3 basis ballsprett.py
@@ -0,0 +1,35 @@
+import pygame as pg
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+VINDU_BREDDE = 500
+VINDU_HOYDE = 500
+FPS = 60
+
+pg.init()
+vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
+clock = pg.time.Clock()
+
+
+def main():
+    running = True
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                running = False
+
+        vindu.fill(WHITE)
+        pg.draw.circle(vindu, RED, (100, 250), 50)
+
+        pg.display.flip()
+        clock.tick(FPS)
+
+    pg.quit()
+
+if __name__ == "__main__":
+    main()
