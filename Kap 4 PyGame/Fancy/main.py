419db29da6e045a116dcc529f4cf90a24b404f73
Rasmus Graver <rasmus.graver@gmail.com>
2025-12-18 09:23:54 +0100

    Update main.py

--- Kap 4 PyGame/Fancy/main.py
+++ Kap 4 PyGame/Fancy/main.py
@@ -15,18 +15,26 @@ def main():
     running = True
 
     while running:
+        jumping = False
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
             elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 running = False
+            elif event.type == pg.KEYDOWN and event.key == pg.K_UP:
+                jumping = True
 
         vindu.fill(WHITE)
-        
+
+
+        # Tegn en fylt rektangel, som viser bakken
+        bakken = pg.Rect(0, BAKKE_HOYDE, VINDU_BREDDE, (VINDU_HOYDE-BAKKE_HOYDE))
+        pg.draw.rect(vindu, GREEN_DARK, bakken)
+
         # Sjekker hvilke taster som er trykket:
         keys = pg.key.get_pressed()
 
-        ninja.update(keys)
+        ninja.update(keys, jumping)
         ninja.draw(vindu)
 
         pg.display.flip()
