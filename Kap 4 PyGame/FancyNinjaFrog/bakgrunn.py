97ec8cc09860b7ed4e978102792a4d63423ada01
Rasmus Graver <rasmus.graver@gmail.com>
2026-01-06 13:17:04 +0100

    Flyttet NinjaFrog dir

--- Kap 4 PyGame/FancyNinjaFrog/bakgrunn.py
+++ Kap 4 PyGame/FancyNinjaFrog/bakgrunn.py
@@ -1,12 +1,13 @@
+from pathlib import Path
 import pygame as pg
 import os
 from constants import *
 
 class Bakgrunn:
-    IMAGE_DIR = os.path.join(os.path.dirname(__file__), "Items", "Boxes", "Box1")
+    IMAGE_DIR = Path(__file__).parent / "Items" / "Boxes" / "Box1"
 
     def __init__(self, x, y):
-        self.image = pg.image.load(os.path.join(self.IMAGE_DIR, "Idle.png"))
+        self.image = pg.image.load(self.IMAGE_DIR / "Idle.png")
 
         # Typisk i PyGame: Bruker en "rect" for å plassere bildet på rett sted på skjermen (se draw metoden)        
         self.rect = self.image.get_rect()
