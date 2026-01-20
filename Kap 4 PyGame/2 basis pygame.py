ba3460dd711441a2420ddea3288eed23742cb73a
Rasmus Graver <rasmus.graver@gmail.com>
2026-01-20 13:00:57 +0100

    basis og ignore

--- Kap 4 PyGame/2 basis pygame.py
+++ Kap 4 PyGame/2 basis pygame.py
@@ -14,23 +14,19 @@ vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 clock = pg.time.Clock()
 
 
-def main():
-    running = True
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-                running = False
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+            running = False
 
-        vindu.fill(WHITE)
-        pg.draw.circle(vindu, RED, (100, 250), 50)
+    vindu.fill(WHITE)
+    pg.draw.circle(vindu, RED, (100, 250), 50)
 
-        pg.display.flip()
-        clock.tick(FPS)
+    pg.display.flip()
+    clock.tick(FPS)
 
-    pg.quit()
+pg.quit()
 
-
-# Kjør programmet vårt:
-main()
