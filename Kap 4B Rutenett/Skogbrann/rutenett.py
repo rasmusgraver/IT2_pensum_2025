5fd4f63f97d9b54064260c97838ad98f0f8de605
Rasmus Graver <rasmus.graver@gmail.com>
2026-01-28 08:50:53 +0100

    skogbrann

---
+++ Kap 4B Rutenett/Skogbrann/rutenett.py
@@ -0,0 +1,77 @@
+import pygame as pg
+from constants import *
+import random
+
+
+class Rute:
+    def __init__(self, rad, kol) -> None:
+        self.rad = rad
+        self.kol = kol
+        self.status = TOM
+        self.nextStatus = TOM
+        self.naboer:list[Rute] = []
+
+
+    def settNaboer(self, brett, antall_rader, antall_kolonner):
+        for r in [-1,0,1]:
+            for k in [-1,0,1]:
+                if not (r==0 and k==0):
+                    # Er vi innafor brettet?
+                    if 0 <= self.rad + r < antall_rader and 0 <= self.kol + k < antall_kolonner:
+                        self.naboer.append(brett[self.rad + r][self.kol + k])
+
+    
+    def nextStep(self):
+        # Finner ut hva "nextStatus" er
+        # tenker det er tryggest å starte med at nextStatus er samme som status:
+        self.nextStatus = self.status
+
+    def nextFrame(self):
+        self.status = self.nextStatus
+
+    def draw(self, vindu):
+        # Tegner fyllt firkant med farge (HVIT/GRØNN/RØD):
+        farge = WHITE
+        if self.status == TRE:
+            farge = GREEN_DARK
+        elif self.status == BRANN:
+            farge = RED
+        pg.draw.rect(vindu, farge, (self.kol * CELLE_STR, self.rad * CELLE_STR, CELLE_STR, CELLE_STR))
+        # Tegner en grå boks rundt (bredde 1) (kjekt når fargen er hvit...)
+        pg.draw.rect(vindu, GREY, (self.kol * CELLE_STR, self.rad * CELLE_STR, CELLE_STR, CELLE_STR), 1)
+
+
+class Rutenett:
+
+
+    def __init__(self, ant_rader, ant_kolonner) -> None:
+        self.ant_rader = ant_rader
+        self.ant_kolonner = ant_kolonner
+        # Lager 2D matrise (liste med lister) der hver verdi er et Rute objekt:
+        self.brett: list[list[Rute]] = [[Rute(r,k) for k in range(self.ant_kolonner)] for r in range(self.ant_rader)]
+        # Sett naboene til rutene:
+        for rad in range(self.ant_rader):
+            for kol in range(self.ant_kolonner):
+                self.brett[rad][kol].settNaboer(self.brett, self.ant_rader, self.ant_kolonner)
+
+
+    def getWindowSize(self) -> tuple:
+        return ( self.ant_kolonner*CELLE_STR, self.ant_rader*CELLE_STR )
+
+
+    def draw(self, vindu):
+        # Kaller på draw metoden til hver Rute:
+        for rad in range(self.ant_rader):
+            for kol in range(self.ant_kolonner):
+                self.brett[rad][kol].draw(vindu)
+
+    def skogbrann(self):
+        # Finn ut "nextStatus" til hver rute:
+        for rad in range(self.ant_rader):
+            for kol in range(self.ant_kolonner):
+                self.brett[rad][kol].nextStep()
+
+        # Oppdater status til "nextStatus":
+        for rad in range(self.ant_rader):
+            for kol in range(self.ant_kolonner):
+                self.brett[rad][kol].nextFrame()
