ce555d72332aed8654b3826c43189ccefbcaf59f
Rasmus Graver <rasmus.graver@gmail.com>
2026-02-05 10:56:29 +0100

    Mario assets

---
+++ Kap 4 PyGame/Mario/mario1.py
@@ -0,0 +1,65 @@
+import pygame as pg
+from constants import *
+
+
+pg.init()
+vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
+clock = pg.time.Clock()
+
+plattfomer = []
+# Legg til 3 plattformer:
+plattfomer.append(  pg.Rect(300, 150, 100, 40)  )
+plattfomer.append(  pg.Rect(100, 280, 150, 40)  )
+plattfomer.append(  pg.Rect(300, 350, 150, 40)  )
+
+# Mario klassen:
+class Mario:
+
+    def __init__(self, x, y) -> None:
+        self.rect = pg.Rect(x, y, 40, 60)
+        # Flytt Mario opp, så han kommer på bakken:
+        self.rect.bottom = y
+
+    def oppdater(self, keys):
+        if keys[pg.K_LEFT]:
+            self.rect.x -= 5
+        if keys[pg.K_RIGHT]:
+            self.rect.x += 5
+
+
+    def tegn(self, vindu):
+        pg.draw.rect(vindu, RED, self.rect)
+
+
+mario = Mario(100, BAKKEN)
+
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+            running = False
+
+    vindu.fill(SKY_BLUE)
+    # Tegn bakken:
+    pg.draw.rect(vindu, GREEN_DARK, (0, BAKKEN, VINDU_BREDDE, (VINDU_HOYDE-BAKKEN) ) )
+
+    # Tegn plattformer:
+    for p in plattfomer:
+        pg.draw.rect(vindu, GREEN_DARK, p)
+
+    # Flytt Mario:
+    keys = pg.key.get_pressed()
+    mario.oppdater(keys)
+
+    # Tegn Mario:
+    mario.tegn(vindu)
+
+
+    pg.display.flip()
+    clock.tick(FPS)
+
+pg.quit()
+
