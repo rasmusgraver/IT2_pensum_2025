0086f216b493362f7cd639bb45b4e85d70cc5487
Rasmus Graver <rasmus.graver@gmail.com>
2026-02-10 15:03:53 +0100

    OOP Simulering

--- Kap 4 PyGame/PlanktonH25/main.py
+++ Kap 4 PyGame/PlanktonH25/main.py
@@ -1,93 +1,25 @@
 import pygame as pg
 from constants import *
-from klasser import Bunndyr, Plankton
-import random
+from simulering import Simulering
 
 
 pg.init()
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 clock = pg.time.Clock()
 
-# Globale variable:
-running = True
-plankton_r_sannsynlighet = 0.02
-plankton_g_sannsynlighet = 0.03
 
-# Sett opp objektene våre:
-bunndyr = Bunndyr()
-planktonliste:list[Plankton] = []
+sim = Simulering()
 
 
-def events():
-    global running
-    global plankton_g_sannsynlighet
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-            running = False
-        elif event.type == pg.KEYDOWN and event.key == pg.K_UP:
-            plankton_g_sannsynlighet += 0.01
-            # Max verdi:
-            if plankton_g_sannsynlighet > 0.2:
-                plankton_g_sannsynlighet = 0.2
-        elif event.type == pg.KEYDOWN and event.key == pg.K_DOWN:
-            plankton_g_sannsynlighet -= 0.01
-            # Minste verdi:
-            if plankton_g_sannsynlighet < 0.01:
-                plankton_g_sannsynlighet = 0.01
+while sim.running:
+    sim.events()
+    sim.random_plankton()
 
-        # Litt debugg info til terminal (uansett hvilken tast man trykker ned):
-        if event.type == pg.KEYDOWN:
-            print(f"Sannsynlighet for grønn er nå {plankton_g_sannsynlighet}.")
-            print(f"Antallet plankton i lista er {len(planktonliste)}")
-
-
-def random_plankton():
-    # Legg til plankton tilfeldig:
-    if random.random() < plankton_g_sannsynlighet:
-        planktonliste.append(Plankton(G))
-    if random.random() < plankton_r_sannsynlighet:
-        planktonliste.append(Plankton(R))
-
-
-def oppdater_objekter():
-    for p in planktonliste[::]:
-        if p.dod:
-            # Fjern døde plankton:
-            planktonliste.remove(p)
-        else:
-            p.oppdater()
-            p.bunndyrKollisjon(bunndyr)
-
-
-def tegn_objekter():
-    bunndyr.tegn(vindu)
-    for p in planktonliste:
-        p.tegn(vindu)
-
-
-def spill_slutt():
-    global running
-
-    # Sjekk om spillet er slutt:
-    if bunndyr.rect.width < 50:
-        print("Bunndyret døde av forgiftning")
-        running = False
-    if bunndyr.rect.width > VINDU_BREDDE:
-        print("Bunndyret døde av forspisning")
-        running = False
-
-
-while running:
-    events()
-    random_plankton()
-
-    # Tegn bakgrunn: (En slags "reset" av hele vinduet vårt)
     vindu.fill(BLACK)
-    oppdater_objekter()
-    tegn_objekter()
-    spill_slutt()
+    sim.oppdater_objekter()
+    sim.tegn_objekter(vindu)
+    sim.spill_slutt()
+
 
     # Har alltid disse med til slutt:
     pg.display.flip()
