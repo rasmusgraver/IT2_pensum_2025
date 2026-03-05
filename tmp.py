4114c3ba9d57585dd190128a5b62b77c1e056bbe
Rasmus Graver <rasmus.graver@gmail.com>
2026-03-05 12:48:45 +0100

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -5,31 +5,35 @@ def tilListe(tall:int) -> list[int]:
     for c in tekst:
         liste.append(int(c))
     return liste
-
-
-
+"""
+# Brukte denne for å teste funksjonen vår:
+tallliste = tilListe(3142)
+if tallliste != [3,1,4,2]:
+    print("NEI! Det ble feil:", tallliste)
+else:
+    print("YES - tilListe funker bra!")
+"""
 
 def fraListe(liste:list[int]) -> int:
     tall = 1000*liste[0] + 100*liste[1] + 10*liste[2] + liste[3]
     return tall
 
-
-
-
-
-
-
-
 """
-tallliste = tilListe(3142)
-if tallliste != [3,1,4,2]:
-    print("NEI! Det ble feil:", tallliste)
+# Brukte denne for å teste funksjonen vår:
+tall = fraListe([3,4,1,7])
+if tall == 3417:
+    print("YES - fraListe funker bra!")
 else:
-    print("YES - bra jobb!")
-
+    print("NEI! Jobbe mer! fraListe returnerte", tall)
 """
 
 
+
+
+"""
+Selve løsningen vår kommer her. De to funksjonene over
+er bare hjelpe-funksjoner vi trenger.
+"""
 def tallspill(tall:int):
     forrigeTall = 0
     while tall != forrigeTall:
