56243d394dfb40394e7d37ee2f98804f90c5b6ee
Rasmus Graver <rasmus.graver@gmail.com>
2026-03-12 11:20:31 +0100

    øveprøve

--- Prøver/MarsØvePrøve/oppg 3 tips.py
+++ Prøver/MarsØvePrøve/oppg 3 tips.py
@@ -10,25 +10,24 @@
 #        ...
 
 
-
 from pathlib import Path
 import csv
 from pprint import pprint
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 CURRENT_DIR = Path(__file__).parent
 filnavn = CURRENT_DIR / "studenter_i_hoyere_utdanning.csv"
-
 
 with open(filnavn, encoding="utf-8-sig") as fil:
     filinnhold = csv.reader(fil, delimiter=";")
     overskrifter = next(filinnhold)
     print(overskrifter)
+    print("Antall overskrifter:", len(overskrifter))
 
     # Lagrer radene i en liste:
     rader = list(filinnhold)
 
 print("Jeg har nå", len(rader), "rader å jobbe med")
-
+print("Antall kolonner i første rad:", len(rader[0]))
+print("Første entries i første rad:", rader[0][0], "|" , rader[0][1], "|" , rader[0][2])
